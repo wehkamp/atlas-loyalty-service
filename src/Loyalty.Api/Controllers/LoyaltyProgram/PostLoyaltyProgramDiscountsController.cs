4ea53e5495822712f12ca4c2ceb8881a36fb4e0a
jroodhardtwehkamp <jroodhardt@wehkamp.nl>
2025-03-28 16:18:20 +0100

    Add apicontroller

---
+++ src/Loyalty.Api/Controllers/LoyaltyProgram/PostLoyaltyProgramDiscountsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Threading.Tasks;
+
+namespace Loyalty.Api.Controllers.AuditRecords;
+
+[ApiController]
+[Route("loyalty-progran")]
+public class PostLoyaltyProgramDiscountsController : ControllerBase
+{
+    [HttpPost]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+    public async Task<IActionResult> PostLoyaltyCodes(
+        string[] discountCodes, string[] customerNumbers, string rfmLevel)
+    {
+        
+
+
+        return Ok("");
+    }
+}
